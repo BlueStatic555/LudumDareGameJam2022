31786b226ba9889e88d2653652da1da22cc5695e
Kyle <reisingkyle07@gmail.com>
2022-04-04 16:23:12 -0400

    Fresh Upload
    plz god

---
+++ GameJam/Assets/Scripts/bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float lifeTime;
+    public float damage;
+    public Rigidbody2D body;
+    int moveSpeed = 20;
+    void Start()
+    {
+        lifeTime = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.AddRelativeForce(new Vector3(moveSpeed, 0, 0));
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0 )
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.tag)
+        {
+            case "R A T":
+                Destroy(this.transform.parent.gameObject); //So we destroy bulletParent, which destroys us too
+                break;
+        }
+    }
+}
